cfb39937f8fe14ee821803ad1051a5ea8146bdf6
Kamalesh-Radhakrishnan <125634379+Kamalesh-Radhakrishnan@users.noreply.github.com>
2023-04-03 18:50:26 +0530

    Update buddyPostController.js

--- exercise-4/controllers/buddyController/buddyPostController.js
+++ exercise-4/controllers/buddyController/buddyPostController.js
@@ -3,11 +3,19 @@ const buddyPostServices = require('../../services/buddyServices/buddyPostService
 // function is async calling a method with Promise.
 const addBuddy = async(req,res,next) => {
     // response is awaited.
-    const response = await buddyPostServices.addBuddy(req.body);
-    if(response.status == 200){
-        res.status(response.status).json(response.data);
+    if(req.body !== null || req.body !== undefined){
+        if(req.body.employeeId !== undefined && req.body.employeeId !== null && req.body.realName !== undefined && req.body.realName !== null && req.body.nickName !== undefined && req.body.nickName !== null && req.body.dob !== undefined && req.body.dob !== null && req.body.hobbies !== undefined && req.body.hobbies !== null){ 
+            const response = await buddyPostServices.addBuddy(req.body);
+            if(response.status == 200){
+                res.status(response.status).json(response.data);
+            }else{
+                res.status(response.status).send(response.message);
+            }
+        }else{
+            res.status(404).send("Invalid parameters and values passed.");
+        }
     }else{
-        res.status(response.status).send(response.message);
+        res.status(404).send("Cannot send null or undefined body.");
     }
 }
 // exporting the controller.
